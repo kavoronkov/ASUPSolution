17343a57380c516ad01763263abe55fb39a9bab9
Kyrylo Voronkov <kavoronkov@gmail.com>
2018-06-27 16:45:13 +0300

    AdminController

---
+++ ASUPWebApplication/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using SeeAllClassLibrary.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASUPWebApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        ISeeAllRepository seeAllRepository;
+
+        public AdminController(ISeeAllRepository repository)
+        {
+            seeAllRepository = repository;
+        }
+
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(seeAllRepository);
+        }
+    }
+}
